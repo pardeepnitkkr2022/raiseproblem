5186323d0713823421bc3c2a4b819a5c17564405
pardeepnitkkr2022 <127744878+pardeepnitkkr2022@users.noreply.github.com>
2024-09-04 00:25:49 +0530

    final commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://raiseproblem-1.onrender.com/api' });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('authToken')) {
