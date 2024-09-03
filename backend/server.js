057947193e47585815be99b0d898a13533c62b7d
pardeepnitkkr2022 <127744878+pardeepnitkkr2022@users.noreply.github.com>
2024-09-04 00:07:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(cors({origin: 'http://localhost:3000'}));
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://pardeepnitkkr2022:Pa%40101556@cluster0.gziurbp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0lhost/raiseproblems", {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
