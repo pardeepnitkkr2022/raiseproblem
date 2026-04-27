aa7e7dc7c492c3c761b91679b68a686c77819456
pardeepnitkkr2022 <127744878+pardeepnitkkr2022@users.noreply.github.com>
2026-04-27 12:24:39 +0530

    Update ai.js

--- backend/routes/ai.js
+++ backend/routes/ai.js
@@ -47,7 +47,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash"
+    model: "gemini-pro"
 });
 
 
